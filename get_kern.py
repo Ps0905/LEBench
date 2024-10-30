970ec37b235a8352507197531969a79e40836ed2
Ps0905 <purnya.samal@rwth-aachen.de>
2024-10-30 15:58:13 +0100

    Update get_kern.py
    Modification to line 15 due to following error:  
        version_seg = num_name.split('-')
    TabError: inconsistent use of tabs and spaces in indentation
    Change of print statements for python3 compatibility

--- get_kern.py
+++ get_kern.py
@@ -1,7 +1,9 @@
 import os
 from subprocess import check_call, check_output, call
+
 fp = open('raw_kern', 'w')
 call(['ls', '/boot'], stdout=fp)
+fp.close()
 
 versions = []
 with open('raw_kern', 'r') as fp:
@@ -9,31 +11,31 @@ with open('raw_kern', 'r') as fp:
     for l in lines:
         if 'vmlinuz' in l:
             full_name = l.strip('vmlinuz-').strip()
-            #print full_name
+            # print full_name
             num_name = full_name.strip('-generic')
             if '-' in num_name:
-		version_seg = num_name.split('-')
+                version_seg = num_name.split('-')
             elif 'c' in num_name:
-            	version_seg = num_name.split('c')
+                version_seg = num_name.split('c')
             elif 'd' in num_name:
-            	version_seg = num_name.split('d')
+                version_seg = num_name.split('d')
 
             first_versions = version_seg[0].split('.')
-            #second_version = version_seg[1]
+            # second_version = version_seg[1]
             ident = []
-	    print version_seg
+            print(version_seg)  # Updated print statement for Python 3
             for s in first_versions:
                 ident.append(int(s))
             length = len(ident)
             assert length == 3
-            #ident.append(int(second_version))
+            # ident.append(int(second_version))
             ident.append(full_name)
             versions.append(ident)
-versions = sorted(versions, key = lambda x : (x[0], x[1], x[2]))
+versions = sorted(versions, key=lambda x: (x[0], x[1], x[2]))
 
 with open('kern_list', 'w') as fp:
     for v in versions:
-        print v
-        fp.write(v[3]+'\n')
+        print(v)  # Updated print statement for Python 3
+        fp.write(v[3] + '\n')
 
 os.remove('raw_kern')
